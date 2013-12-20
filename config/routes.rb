49feb60440643e992ac20ffeecf519d42107b8a6
Andrew Lin <f50ajl@gmail.com>
2013-12-19 20:26:40 -0500

    re-add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Currently::Application.routes.draw do
 
   root to: "static#home"
 
+  devise_for :users
+
   match 'auth/:provider/callback', to: 'sessions#create', via: :get
   match 'auth/failure', to: redirect('/'), via: :get
   match 'signout', to: 'sessions#destroy', as: 'signout', via: :get
