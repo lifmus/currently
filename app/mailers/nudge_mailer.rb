f9896dd59649f420b0581bdb9bc3ba3acd71b83c
Andrew Lin <f50ajl@gmail.com>
2014-01-08 14:57:53 -0500

    nudge override for development

--- app/mailers/nudge_mailer.rb
+++ app/mailers/nudge_mailer.rb
@@ -5,12 +5,23 @@ class NudgeMailer < MandrillMailer::TemplateMailer
   def nudge(sender, recipient)
     mandrill_mail template: 'Nudge User',
                   subject: sender.name + ' wants you to update your status',
-                  to: 'andrew@andrewlin.net', #change this later
+                  to: correct_recipient(recipient),
+                  bcc: 'admin@usecurrently.com',
                   vars: {
                     'EMAIL_MESSAGE' => sender.name + " nudged you on Currently. Update your status to let them know what you've been up to!" ,
                     'EMAIL_BUTTON_ADDRESS' => 'http://www.usecurrently.com/friends',
                     'EMAIL_BUTTON_MESSAGE' => 'Update Now'
                   },
                   inline_css: true
+  end
+
+  private
+
+  def correct_recipient(recipient)
+    if Rails.env.development? 
+      'nudge_override@andrewlin.net'
+    else
+      recipient.email
+    end
   end
 end
